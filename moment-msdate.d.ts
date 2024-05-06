03a6aa440f88495ff129f3e512d87c03ebde4386
Dave Sullivan <dave@davesullivan.ca>
2024-05-06 18:38:36 -0400

    Add Typescript definitions

---
+++ moment-msdate.d.ts
@@ -0,0 +1,11 @@
+import moment = require('moment')
+
+declare module 'moment' {
+  interface Moment {
+    toOADate(): number
+  }
+
+  const fromOADate = (oaDate: number, offset: number | string): Moment => {}
+}
+
+export = moment
